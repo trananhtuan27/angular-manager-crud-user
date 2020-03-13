b4f8497dcac6b59dc44e791fc4fe61e0962bdc65
trananhtuan27 <trananhtuan2710201@gmail.com>
2020-03-13 15:21:48 +0700

    heroku

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appId: '150972784ac28fb',
+  apiKey: '0d3c7685a66741901bb196d08a460f7bb24640fb',
+  region: 'us'
+};
